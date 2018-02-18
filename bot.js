5a8808f2e49971273f372ae7acc209c084b44c47
Aaron Huang <aaronhuangwork@gmail.com>
2018-02-17 16:34:50 -0800

    moved command cases outward into helper functions

--- bot.js
+++ bot.js
@@ -7,29 +7,45 @@ bot.on("ready", () => {
     console.log("Mountain bot here.");
 })
 
+// List of Commands
+
+function ping(message) {
+    message.channel.send("pong!");
+}
+
+function join(message) {
+    if (!message.guild) return;
+    if (message.member.voiceChannel) {
+        message.member.voiceChannel.join()
+        .then(connection => { // Connection is an instance of VoiceConnection
+            message.reply("I have successfully connected to the channel!");
+            })
+            .catch(console.log);
+    } else {
+        message.reply("You are not in a voice channel.");
+    }
+}
+
+function handle_command(cmd, message) {
+    switch (cmd) {
+        case "ping":
+            ping(message);
+            break;
+        case "join":
+            join(message);
+            break;
+        break;
+    }
+}
+
+// Bot Events
+
 bot.on("message", (message) => {
     if (message.content.startsWith("!")) {
         var arg = message.content.substring(1).split(" ");
         var cmd = arg[0];
 
-        switch (cmd) {
-            case "ping":
-                message.channel.send("pong!");
-                break;
-            case "join":
-                if (!message.guild) return;
-                if (message.member.voiceChannel) {
-                    message.member.voiceChannel.join()
-                    .then(connection => { // Connection is an instance of VoiceConnection
-                        message.reply("I have successfully connected to the channel!");
-                      })
-                      .catch(console.log);
-                } else {
-                    message.reply("You are not in a voice channel.");
-                }
-                break;
-            break;
-        }
+        handle_command(cmd, message);
     }
 });
 
