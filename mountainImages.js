0a22aa48ed4c8b1cf22b1ad728fda24e1c83cd6e
Shang Mak <shangmak@d207-023-197-038.wireless.sfu.ca>
2018-02-17 20:10:07 -0800

    remove google packages

--- mountainImages.js
+++ mountainImages.js
@@ -1,14 +1,1 @@
 
-// CSE ID : 008030941676365421230:jptivdegq0w
-// API KEY :AIzaSyBN2S7JKaiww6s6BQM8FRBjf-0UgZv1jq8
-
-const cse_ID  = '005832064830925815114:e_xlpswsc5e';
-const API_Key = 'AIzaSyCY1vES20Cxn7pYPOjob0amdf9x-mWGbVs';
-const GoogleImages = require('google-images');
- 
-const client = new GoogleImages(cse_ID, API_Key);
-
-const images = client.search('Aaron Huang');
-
-console.log(images[1] == null);
-
