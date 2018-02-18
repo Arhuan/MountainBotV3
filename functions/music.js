967de0072468967ff1bce8cbca48a05d98c9af9d
Aaron Huang <aaronhuangwork@gmail.com>
2018-02-18 10:04:16 -0800

    voiceconnection resets after play is done

--- functions/music.js
+++ functions/music.js
@@ -60,6 +60,7 @@ module.exports = {
                         this.play(message);
                     } else {
                         voiceconnection.disconnect();
+                        voiceconnection = null;
                     }
                 });
             }
