8b74b760cf23b300ae833751e0a5e6646da0ae11
Aaron Huang <aaronhuangwork@gmail.com>
2018-02-17 19:21:25 -0800

    added remove function for music

--- bot.js
+++ bot.js
@@ -55,6 +55,9 @@ function handle_command(cmd, message, target) {
             if (target != null) 
                 music.add(message, target);
             break;
+        case "remove":
+            music.remove(message);
+            break;
         case "play":
             music.play(message);
             break;
